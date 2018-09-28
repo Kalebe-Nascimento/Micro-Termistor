13302a6fa107ed48477852469634c1d135ef5e69
Kalebe <38252510+kalebe2001@users.noreply.github.com>
2018-09-28 10:03:21 -0300

    Projeto final do termistor+LCD

---
+++ terms_displayGGWP.ino
@@ -0,0 +1,30 @@
+#include <LiquidCrystal.h>
+#include <Thermistor.h>
+ 
+Thermistor temp(1); 
+ 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+ 
+void setup() {
+Serial.begin(9600);
+lcd.begin(16, 2);
+}
+ 
+void loop() {
+
+float temperature = temp.getTemp(); 
+
+Serial.print("Temperatura: ");
+Serial.print(temperature);
+Serial.println("°C");
+ 
+//imprimindo no LCD
+lcd.setCursor(0,0); 
+lcd.print("Temp = ");
+lcd.print(temperature);
+lcd.write(B11011111); 
+lcd.print("C");
+ 
+delay(1000);
+}
+
